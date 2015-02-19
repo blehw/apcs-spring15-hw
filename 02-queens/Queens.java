9950bae692f3803fffaef858b5ae7f31d81cce5c
blehw <winton2095@gmail.com>
2015-02-18 21:09:44 -0500

    stack overflow error

--- 02-queens/Queens.java
+++ 02-queens/Queens.java
@@ -10,7 +10,7 @@ public class Queens {
     private char space = '.';
     private char attack = 'x';
     private char queen = 'q';
-
+    private boolean us = false;
     public void delay(int n){
 	try {
 	    Thread.sleep(n);
@@ -52,14 +52,27 @@ public class Queens {
 	return s;
     }
 
-    public void solve(int n, int m) {
+    public boolean unsolvable() {
+	for (int i=0;i<maxX;i++) {
+	    for (int k=0;k<maxY;k++) {
+		if (board[i][k] == '.') {
+		    return false;
+		}
+	    }
+	}
+	return true;
+    }
+
+    public void solve(int n) {
 	
 	Random rand = new Random();
+	int x = rand.nextInt(maxX);
+	int y = rand.nextInt(maxY);
 	
-	while (n != 0) {
+	if (n != 0) {
 	    
 	    if (board[x][y] == attack) {
-		return;
+		solve(n,n);
 	    } 
 	    if (board[x][y] == space) {
 		for (int i=0;i<maxY;i++) {
@@ -70,7 +83,7 @@ public class Queens {
 		}
 		int a = x;
 		int b = y;
-		while (a+1=<maxX && b+1=<maxY) {
+		while (a+1<maxX && b+1<maxY) {
 		    a = a + 1;
 		    b = b + 1;
 		    board[a][b] = attack;
@@ -84,26 +97,96 @@ public class Queens {
 		}
 		int e = x;
 		int f = y;
-		while (e+1=<maxX && f-1>=0) {
+		while (e+1<maxX && f-1>=0) {
 		    e = e + 1;
 		    f = f - 1;
 		    board[e][f] = attack;
 		}
 		int g = x;
 		int h = y;
-		while (g-1>=0 && h+1=<maxY) {
+		while (g-1>=0 && h+1<maxY) {
 		    g = g - 1;
 		    h = h + 1;
 		    board[g][h] = attack;
 		}
 		board[x][y] = queen;
+		solve(n-1,n);
 	    }
-	    
-	    delay(1000);
-	    System.out.println(this);
-	    solve(rand.nextInt(maxX), rand.nextInt(maxY));
+	    if (unsolvable()) {
+	        for (int i=0;i<maxX;i++) {
+		    for (int k=0;k<maxY;k++) {
+			board[i][k] = '.';
+			solve(n,n);
+		    }
+		}
+	    }
    
-	}	
+	} else {
+	    System.out.println(this);
+	}		
+    }
+
+    public void solve(int n, int m) {
+	
+	Random rand = new Random();
+	int x = rand.nextInt(maxX);
+	int y = rand.nextInt(maxY);
+	
+	if (n != 0) {
+	    
+	    if (board[x][y] == attack) {
+		solve(n,m);
+	    } 
+	    if (board[x][y] == space) {
+		for (int i=0;i<maxY;i++) {
+		    board[x][i] = attack;
+		}
+		for (int k=0;k<maxX;k++) {
+		    board[k][y] = attack;
+		}
+		int a = x;
+		int b = y;
+		while (a+1<maxX && b+1<maxY) {
+		    a = a + 1;
+		    b = b + 1;
+		    board[a][b] = attack;
+		}
+		int c = x;
+		int d = y;
+		while (c-1>=0 && d-1>=0) {
+		    c = c - 1;
+		    d = d - 1;
+		    board[c][d] = attack;
+		}
+		int e = x;
+		int f = y;
+		while (e+1<maxX && f-1>=0) {
+		    e = e + 1;
+		    f = f - 1;
+		    board[e][f] = attack;
+		}
+		int g = x;
+		int h = y;
+		while (g-1>=0 && h+1<maxY) {
+		    g = g - 1;
+		    h = h + 1;
+		    board[g][h] = attack;
+		}
+		board[x][y] = queen;
+		solve(n-1,m);
+	    }
+	    if (unsolvable()) {
+	        for (int i=0;i<maxX;i++) {
+		    for (int k=0;k<maxY;k++) {
+			board[i][k] = '.';
+			solve(m,m);
+		    }
+		}
+	    }
+   
+	} else {
+	    System.out.println(this);
+	}
 		
     }
 
