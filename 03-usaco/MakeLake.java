16a5f2c611e06e03acc28d3f796031a96b22e52b
Winton Yee <wyee@github.com>
2015-02-24 08:42:00 -0500

    i hate this

--- 03-usaco/MakeLake.java
+++ 03-usaco/MakeLake.java
@@ -2,25 +2,52 @@ import java.io.*;
 import java.util.*;
 
 public class MakeLake {
-
+    
     private int R = 4;
     private int C = 6;
     private int E = 22;
     private int N = 2;
-    private int[][] lake;
+    private int[][] lake = new int[C][R];
+    private int[][] instructions = new int[N][3];
 
     public MakeLake() {
-        try {					
-	    Scanner sc = new Scanner(new File("makelake.dat"));
-	} catch (Exception e) {}
-	int j=0;
-	while (sc.hasNext()) {
-	    String line = sc.nextLine();
-	    for (int i=0;i<maxX;i++) {
-		lake[i][j] = line.intAt(i);
+	int [] a = {28,25,20,32,34,36,27,25,20,20,30,34,24,20,20,20,20,30,20,20,14,14,20,20};
+	instructions[0] = {1,4,4};
+	instructions[1] = {1,1,10};
+	int i = 0;
+	for (int r=0;r<R;r++) {
+	    for (int c=0;c<C;c++) {
+		lake[c][r] = a[i];
+		i++;
 	    }
-	    j++;
 	}
     }
+
+    public String toString() {
+	String s = "";
+	for (int r=0;r<R;r++) {
+	    for (int c=0;c<C;c++) {
+		s = s + lake[c][r] + " ";
+	    }
+	    s = s + "\n";
+	}
+	return s;
+    }
+    
+    public void stomp() {
+	int x;
+	int y;
+	for (int i=0;i<N;i++) {
+	    x = instructions[N][0];
+	    y = instructions[N][1];
+	    
+	}
+    }
+    
+    
+    public static void main(String[] args) {
+	MakeLake m = new MakeLake();
+	System.out.println(m);
+   
 
 }
